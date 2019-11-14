88c9ba70c7e30cda416cd743303223b9f701f6b3
4012Robotics <bnb4012@gmail.com>
2019-11-14 16:50:42 -0500

    status changes

--- bot.js
+++ bot.js
@@ -1,12 +1,23 @@
 // Required Declarations
 const Discord = require('discord.js');
 const fs = require('fs');
-// End required Declaration
 
-//Client Setup 
 
+// Bot Declaration
 const Bot = new Discord.Client({
     disableEveryone: true 
 });
 
-Bot.login()
+Bot.config = require('./config.js');
+Bot.login(Bot.config.token);
+
+// End required declarations 
+
+
+Bot.on('ready', e => {
+    console.log("4012-HELPER is starting.");
+    console.log("Setting the status to the one set in the bot configuration.");
+    bot.user.setStatus(Bot.config.status, {type: WATCHING} );
+    //- To do: Add switching status from an array defined in the config. 
+    console.log("The status has been set. ")
+})
